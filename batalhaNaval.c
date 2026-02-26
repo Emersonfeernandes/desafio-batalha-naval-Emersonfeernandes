3862d10c05ce13d5f58948f8930bc7559b59f94d
Emersonfeernandes <80550746+Emersonfeernandes@users.noreply.github.com>
2026-02-26 01:02:30 +0000

    modified:   batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,12 +3,138 @@
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
+char *navio(int index1, int index2){
+    char *tabuleiro[10][10] ={
+        {" N   ", " A   ", " V   ", " I   ", " O   ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " N   ", " A   ", " V   ", " I   ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "}  
+    };
+
+    return tabuleiro[index1][index2];
+}
+void funcTabuleiro(char *tabuleiro[10][10]){
+    int i = 0;
+
+    // Exibindo o tabuleiro
+    printf("      A    B    C    D    E    F    G    H    I    J\n");
+    do {
+        printf("\n%d - ", i);
+        for(int j = 0; j < 10; j++) {
+            printf("%s", tabuleiro[i][j]);
+        }
+        i++;
+    } while (i < 10);
+}
+
+void player(char *tabuleiro[10][10], int index, char letra){
+    
+    int i = 0;
+    //tabuleiro[index][2] = " M   ";
+
+    switch (letra)
+    {
+    case 'A':
+    case 'a':
+        tabuleiro[index][0] = navio(index, 0);
+        break;
+
+    case 'B':
+    case 'b':
+        tabuleiro[index][1] = navio(index, 1);
+        break;
+
+    case 'C':
+    case 'c':
+        tabuleiro[index][2] = navio(index, 2);
+        break;
+
+    case 'D':
+    case 'd':
+        tabuleiro[index][3] = navio(index, 3);
+        break;
+
+    case 'E':
+    case 'e':
+        tabuleiro[index][4] = navio(index, 4);
+        break;
+
+    case 'F':
+    case 'f':
+        tabuleiro[index][5] = navio(index, 5);
+        break;
+    
+    case 'G':
+    case 'g':
+        tabuleiro[index][6] = navio(index, 6);
+        break;
+
+    case 'H':
+    case 'h':
+        tabuleiro[index][7] = navio(index, 7);
+        break;
+    
+    case 'I':
+    case 'i':
+        tabuleiro[index][8] = navio(index, 8);
+        break;
+
+    case 'J':
+    case 'j':
+        tabuleiro[index][9] = navio(index, 9);
+        break;
+    
+    default:
+        break;
+    }
+    
+
+    // Exibindo o tabuleiro
+    printf("\n     A    B    C    D    E    F    G    H    I    J\n");
+    do {
+        printf("\n%d - ", i);
+        for(int j = 0; j < 10; j++) {
+            printf("%s", tabuleiro[i][j]);
+        }
+        i++;
+    } while (i < 10);
+}
 
 int main() {
+    char *tabuleiro[10][10] ={
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "},
+        {" Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar ", " Mar "}  
+    };
+    int index;
+    char letra;
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    
+    funcTabuleiro(tabuleiro);
+    do
+    {
+        printf("\nDigite um número de 0 a 1 para lonha e um letra e A a J para coluna\n");
+        scanf("%d %c", &index, &letra);
+        player(tabuleiro, index, letra);
+    
+    }while (letra != 'S' && letra != 's');
+    
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
